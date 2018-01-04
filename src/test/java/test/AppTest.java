473b87447f7218c7d0a1391026fd7baea513fc80
rahul singh <rahul16j1990@gmail.com>
2018-01-05 00:12:18 +0530

    this is my first code commit

---
+++ src/test/java/test/AppTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+public void myTest(){
+    System.out.println("This is just test class");
+}
+}
